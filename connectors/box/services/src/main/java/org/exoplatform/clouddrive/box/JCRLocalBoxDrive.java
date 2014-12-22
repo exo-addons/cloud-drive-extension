b025cbc0bd28ed3e41fd7e4f8389c35c8599a5e2
Peter Nedonosko <peter.nedonosko@exoplatform.com>
2014-12-22 03:15:51 +0200

    #26 file syncing CSS; improved local to cloud sync to reduce number of updates for single file; improved error handling with JS client

--- connectors/box/services/src/main/java/org/exoplatform/clouddrive/box/JCRLocalBoxDrive.java
+++ connectors/box/services/src/main/java/org/exoplatform/clouddrive/box/JCRLocalBoxDrive.java
@@ -52,7 +52,6 @@ import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.text.ParseException;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Iterator;
